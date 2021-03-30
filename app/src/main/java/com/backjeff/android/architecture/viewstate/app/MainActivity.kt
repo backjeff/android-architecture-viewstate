3467b2fed57bc78de3631eef7752c8194ea052fa
Jefferson Rodrigues <jefferson.rodrigues.mail@gmail.com>
2021-03-30 03:50:02 -0300

    Fix some publishing issues

--- app/src/main/java/com/backjeff/android/architecture/viewstate/app/MainActivity.kt
+++ app/src/main/java/com/backjeff/android/architecture/viewstate/app/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.backjeff.android.architecture.viewstate
+package com.backjeff.android.architecture.viewstate.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.backjeff.android.architecture.viewstate.ViewStateListener
 
 class MainActivity : AppCompatActivity(), ViewStateListener {
     override fun onCreate(savedInstanceState: Bundle?) {
